3ee4a1168424c7c16809cba9ce1bf4ed708facf8
Bradford Powell <bpow@drpowell.org>
2015-04-01 20:59:34 -0400

    avoid a lot of extra io

--- ExCID_v2.0/check_HGNC_individual_CCDSDB.pl
+++ ExCID_v2.0/check_HGNC_individual_CCDSDB.pl
@@ -5,9 +5,17 @@ use strict;
 my $annotated_index = $ARGV[0];
 my $HGNC = $ARGV[1];
 
+my %words_to_genes = (); # not a perfect index, but will do the same as "grep -w"
+open(my $hgnc_fh, "<$HGNC") or die $!;
+while (my $line = <$hgnc_fh>) {
+    chomp $line;
+    my @row = split(/[\s,]/, $line);
+    my $gene = shift @row;
+    map { $words_to_genes{$_} = $gene } @row;
+}
+close $hgnc_fh or die $!;
 
 open(my $fh,"<$annotated_index") or die $!;
-
 
 while (my $line = <$fh>) {
     
@@ -16,18 +24,9 @@ while (my $line = <$fh>) {
     my ($chr,$start,$Stop,$ID) = split("\t",$line);
     my @transcript_ID_split = split("_exon_",$ID);
     my $transcript_ID = $transcript_ID_split[0];
-    
-    my @grep = `grep -w "$transcript_ID" $HGNC `;
-    
-    if (scalar(@grep) == 1) {
-        my @tmp = split("\t",$grep[0]);
-        my $gene_name = $tmp[0];
-        print "$chr\t$start\t$Stop\t$ID\t$gene_name\n";
-    }else{
-        print "$chr\t$start\t$Stop\t$ID\t.\n";
-        #print STDERR "$chr\t$start\t$Stop\t$ID\t.\n";
-    }
-    
+
+    my $gene_name = $words_to_genes{$transcript_ID} || '.';
+    print "$chr\t$start\t$Stop\t$ID\t$gene_name\n";
 }
 
 close($fh);
--- ExCID_v2.0/check_HGNC_individual_RefSeqDB.pl
+++ ExCID_v2.0/check_HGNC_individual_RefSeqDB.pl
@@ -5,6 +5,15 @@ use strict;
 my $annotated_index = $ARGV[0];
 my $HGNC = $ARGV[1];
 
+my %words_to_genes = (); # not a perfect index, but will do the same as "grep -w"
+open(my $hgnc_fh, "<$HGNC") or die $!;
+while (my $line = <$hgnc_fh>) {
+    chomp $line;
+    my @row = split(/[\s,]/, $line);
+    my $gene = shift @row;
+    map { $words_to_genes{$_} = $gene } @row;
+}
+close $hgnc_fh or die $!;
 
 open(my $fh,"<$annotated_index") or die $!;
 
@@ -17,16 +26,9 @@ while (my $line = <$fh>) {
     my @transcript_ID_tmp_split = split("_exon_",$ID);
     my $transcript_ID = $transcript_ID_tmp_split[0];
 
-    my @grep = `grep -w "$transcript_ID" $HGNC `;
+	my $gene_name = $words_to_genes{$transcript_ID} || '.';
     
-    if (scalar(@grep) == 1) {
-        my @tmp = split("\t",$grep[0]);
-        my $gene_name = $tmp[0];
-        print "$chr\t$start\t$Stop\t$gene\t$ID\t$gene_name\n";
-    }else{
-        print "$chr\t$start\t$Stop\t$gene\t$ID\t.\n";
-       # print STDERR "$chr\t$start\t$Stop\t$gene\t$ID\t.\n";
-    }
+	print "$chr\t$start\t$Stop\t$gene\t$ID\t$gene_name\n";
     
 }
 
